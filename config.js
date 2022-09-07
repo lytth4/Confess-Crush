f1226b1c8a115b314477de8c439e4211c14fa550
lytth4 <112964785+lytth4@users.noreply.github.com>
2022-09-07 23:34:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    titleWeb: "UNICORN TEAM",
+    introTitle: 'Hi cậu',
+    introDesc: `Chào mừng cậu đã đến với 
+    thế giới của UNICORN`,
+    btnIntro: 'START',
+    title: 'Ngày hôm nay của cậu thế nào 🥰',
+    btnYes: 'Rất rất vuiiii',
+    btnNo: 'Hơi mệt áaaa',
+    question: 'Chuẩn bị kì công thế này không vui sao được. Có điều gì muốn nói khum?',
+    btnReply: 'Gửi',
+    reply: 'Cảm động quá mất thôi. Yêu thương rất nhèooooo <3 <3',
+    mess: 'Mình biết mà 🥰. Không có gì đâu 😘😘',
+    messDesc: 'Giờ thì tạm quên áp lực doanh số mà nghe những lời chúc này nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://drive.google.com/file/d/1Fppb0SX7CR0fEoVbpOFpjtJs8aQ6Xkt4/view?usp=sharing' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
